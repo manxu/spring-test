a3d94e51ac2d85177f9325862ebb664ba94ff658
zhangcheng <zhangcheng@cangoonline.com>
2021-08-20 17:49:55 +0800

    git

--- springboot-test/src/main/java/com/zc/controller/UserController.java
+++ springboot-test/src/main/java/com/zc/controller/UserController.java
@@ -1,15 +1,36 @@
 package com.zc.controller;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.zc.dao.UserDao;
+import com.zc.util.HttpUtil;
+import com.zc.util.MapUtil;
 
 @Controller
 public class UserController {
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserDao userDao;
 
@@ -19,10 +40,111 @@ public class UserController {
     }
 
 
-    @RequestMapping("/users")
+    @RequestMapping("/collections")
     @ResponseBody
-    public Object getUsers(){
-        return userDao.getUser();
+    public Object getCol(){
+        Map<String, Object> param = new HashMap<>();
+        param.put("env", "zc-hongbao-3grj4o8d48f4a1f2");
+        param.put("limit", 50);
+        String s = HttpUtil.
+                sendPost("https://api.weixin.qq.com/tcb/databasecollectionget?access_token=" + HttpUtil.token, param);
+        return s;
+    }
+
+
+    @RequestMapping("/datas")
+    @ResponseBody
+    public Object getCol(@RequestParam String table,@RequestParam String para,@RequestParam String skip){
+        Map<String,Object> param =  new MapUtil()
+                .com("env", HttpUtil.getKey("env"))
+                .com("query", "db.collection(\""+ table +"\").where("+para+").limit(50).skip("+skip+").get()")
+                .map;
+        String s = HttpUtil.sendPost(HttpUtil.QUERY, param);
+        return s;
+    }
+
+    //更新
+    @RequestMapping("/update")
+    @ResponseBody
+    public Object update(@RequestParam String table,@RequestParam String para){
+        logger.info("update : " + para);
+        JSONObject p = JSONObject.parseObject(para);
+        String id = p.getString("_id");
+        p.remove("_id");
+        Map<String,Object> param =  new MapUtil()
+                .com("env", HttpUtil.getKey("env"))
+                .com("query", "db.collection(\""+ table +"\").where({\"_id\":\""+ id +"\"}).update({data:"+ p.toJSONString() +"})")
+                .map;
+        String s = HttpUtil.sendPost(HttpUtil.UPDATE, param);
+        logger.info(s);
+        return s;
+    }
+
+
+    @RequestMapping("/1")
+    public String order(){
+        return "upload";
+    }
+
+    //更新
+    @RequestMapping("/upload")
+    @ResponseBody
+    public Object upload(@RequestParam MultipartFile file){
+        BufferedInputStream is = null;
+        List<Map<String,Object>> res = new ArrayList<>();
+        try {
+            is = new BufferedInputStream(
+                    file.getInputStream());
+            Workbook workbook = new XSSFWorkbook(is);
+            Sheet sheet = workbook.getSheetAt(0);
+            int rows = sheet.getPhysicalNumberOfRows();
+            //读第一个sheet
+            for (int i = 1;i<rows;i++){
+                Row row = sheet.getRow(i);
+                String no = row.getCell(10).getStringCellValue();
+                String amount = row.getCell(12).getStringCellValue();
+
+                //查询
+                String query = "db.collection(\"m_order\").aggregate()." +
+                        "lookup({" +
+                        "from:'m_user'," +
+                        "localField:'_openid'," +
+                        "foreignField:'_openid'," +
+                        "as:'user'" +
+                        "}).where({no:'"+ no +"'}).get()";
+                Map<String,Object> param =  new MapUtil()
+                        .com("env", HttpUtil.getKey("env"))
+                        .com("query", query)
+                        .map;
+                String cq = HttpUtil.sendPost(HttpUtil.QUERY, param);
+                JSONObject cqjson = JSONObject.parseObject(cq);
+                for(int j=0;j< cqjson.getJSONArray("data").size();j++){
+                    JSONObject c = cqjson.getJSONArray("data").getJSONObject(j);
+                    if(c.get("fl")!=null){
+                        res.add(new MapUtil().com("no", no).com("amount", c.getString("amount")).com("fl", c.getString("fl"))
+                                .com("flflag","已返利").map);
+                    }else{
+
+                        /*Map<String,Object> be = new MapUtil().com("no", no).com("amount",  "").
+                                com("fl",  "")).map;*/
+                        //be.put()
+
+
+
+                    }
+                }
+
+
+
+
+
+            }
+        } catch (IOException e) {
+            logger.error("", e);
+        }finally {
+            is = null;
+        }
+        return res;
     }
 
 }
--- springboot-test/src/main/webapp/WEB-INF/jsp/index.jsp
+++ springboot-test/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -5,10 +5,99 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Insert title here</title>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+    <style>
+        table, td, th {
+            border: 1px solid black;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th {
+            height: 70px;
+        }
+        .cc {
+            width: 400px;
+            margin-top: 5px;
+            margin-left: 5px;
+        }
+    </style>
+    <script type="text/javascript">
+        $(function (){
+            var xx = {};
+            $.getJSON('/collections',{},function (data){
+                for(var i in data.collections){
+                    $("#col").append('<li class="datas">'+ data.collections[i].name +'</li>');
+                }
+                $(".datas").click(function (){
+                    var table = $(this).text();
+                    var skip = $("#skip").val();
+                    $.ajax({type:'POST',url:'datas',dataType:'json',data:{'table':table,'para':"{}", 'skip': skip},
+                    success:function (res){
+                        $('#list').html("");
+                        $('#detail').html("");
+                        for(var i in res.data){
+                            var x = JSON.parse(res.data[i]);
+                            $("#list").append('<p id="'+x._id+'">' + i + ': ' + x._id +'</p>');
+                            xx[x._id] = res.data[i];
+                        }
+                        $('#list>p').click(function (){
+                            var id  = $(this).attr("id");
+                            var x = JSON.parse(xx[id]);
+                            $('#detail').html("");
+                            for(var s in x){
+                                var type = typeof x[s] == 'number' ? 1 : 0;
+                                var value = ((typeof x[s] == 'object' ? JSON.stringify(x[s]) : x[s]));
+                                var str = '<span>'+ s + ':</span> <input class="cc" is_number="'+type+'" name="'+ s +'"  data_table="'+ table +'"  data_id="'+ id +'" value=\'' + value + '\'> </input>';
+                                str += '<input type="button" value="修改" class="modify"/></br>';
+                                $('#detail').append(str);
+                            }
+                            $(".modify").click(function (){
+                                var inp = $(this).prev();
+                                var n = inp.attr('is_number')
+                                $.ajax({
+                                    type:'POST',
+                                    url:'update',
+                                    dataType:'json',
+                                    data:{'table':inp.attr('data_table'),'para':'{"_id":"'+ inp.attr('data_id')+'", "'+ inp.attr('name') +'":' + (n==1?'':'"')   + inp.val() + (n==1?'':'"') +'}'},
+                                    success:function(res){
+                                        alert(res.errcode + ":" + res.errmsg)
+                                    }
+                                });
+                            });
+                        });
+                    }});
+                });
+            })
+
+
+
+        });
+    </script>
+
 </head>
 <body>
-Hi JSP.
-<hr>
-<!--测试发现更改jsp文件,也需要重启启动类(清缓存也不行!) -->
+skip <input type="text" value="0" id="skip">
+<div style="display: flex; flex-direction: row">
+    <div style="width: 20%; border: 1px solid gray">
+        表空间
+        <ul id="col" style="width:200px;">
+
+        </ul>
+    </div>
+    <div style="width: 20%; border: 1px solid gray" id="list">
+
+    </div>
+    <div style="width: 60%; border: 1px solid gray" id="detail">
+
+    </div>
+</div>
+
+
+
+
 </body>
 </html>
